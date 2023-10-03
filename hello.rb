6870d882e5adb98b3d43c11ea168fe8c06d8cd75
Luis Tejeda <46000487+LuisTejedaS@users.noreply.github.com>
2023-10-03 15:25:24 -0600

    first program

--- hello.rb
+++ hello.rb
@@ -1,1 +1,38 @@
 puts "hello"
+puts "hello".size
+puts "hello".upcase
+puts "UPERCASED".downcase
+puts "hello".gsub("llo", "lla")
+puts "hello".chars
+puts "1".to_i + 2
+
+
+age = 20
+name = "Luis"
+
+puts "Hello #{name}, our records tell us that you're #{age} years old!"
+
+numbers = []
+
+numbers << 1
+numbers << 2
+numbers << 3
+
+print numbers
+puts numbers.size
+
+ip_to_domain = { "rubyguides.com" => "185.14.187.159" }
+
+ip_to_domain["rubyguides.com"] = "8.8.8.8"
+puts ip_to_domain["rubyguides.com"]
+
+unless 2 > 3
+    puts("dont get confuse")
+end
+
+#read
+p gets
+
+#removes the newline char
+name = gets.chomp
+p name
